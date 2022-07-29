756f8c558fc60b8bb308b3cab49f93156c9cc653
Ola4606 <olaoluwaadeyemo7@gmail.com>
2022-07-29 15:02:30 +0100

    Loading Screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Arrows from "./components/arrows";
 import Bio from "./components/bio";
 import ImgProfile from "./components/imgprofile";
+import Loading from "./components/loading";
 import NavigationBar from "./components/navigationBar";
 import { getBillionaireData } from "./functions/getBillionaireData";
 
@@ -9,21 +10,18 @@ let currentCount = 0;
 let allBillionaireData: [] = [];
 
 function App() {
-
   let [currentBillionaireData, setCurrentBillionaireData] = useState({});
   // let [allBillionaireData, setAllBillionaireData] = useState([]);
-  
+
   let [loadingPageState, setLoadingPageState] = useState(0); //0- loading | 1-has data | 2-no internet
   let [isReadMore, setIsReadMore] = useState(false);
   // let [currentCount, setCurrentCount] = useState(0);
-  
 
   useEffect(() => {
     async function getData() {
       const data = await getBillionaireData();
 
       if (data !== null) {
-       
         // setAllBillionaireData(data);
         allBillionaireData = data;
         console.log(allBillionaireData);
@@ -35,74 +33,64 @@ function App() {
         setLoadingPageState(1);
         // console.log('done3');
         // console.log(loadingPageState);
-
-      } else{
-        setLoadingPageState(2)
+      } else {
+        setLoadingPageState(2);
       }
-
     }
 
     getData();
   }, []);
-
 
   function toggleReadMore() {
     setIsReadMore((prevIsReadMore) => !prevIsReadMore);
   }
 
   function onClickBack() {
-
-    if(currentCount !== 0) {
-
-      setCurrentBillionaireData(allBillionaireData[(currentCount - 1)]);
-      currentCount--
-      if(isReadMore) {
+    if (currentCount !== 0) {
+      setCurrentBillionaireData(allBillionaireData[currentCount - 1]);
+      currentCount--;
+      if (isReadMore) {
         setIsReadMore(false);
       }
-      
     }
-    
   }
-  
+
   function onClickNext() {
-   
-    if(currentCount !== 99) {
-
-      setCurrentBillionaireData(allBillionaireData[(currentCount + 1)]);
-      currentCount++
-      if(isReadMore) {
+    if (currentCount !== 99) {
+      setCurrentBillionaireData(allBillionaireData[currentCount + 1]);
+      currentCount++;
+      if (isReadMore) {
         setIsReadMore(false);
       }
-      
-      
     }
-    
   }
 
-  
- console.log("a");
- console.log(currentCount);
-  
+  console.log("a");
+  console.log(currentCount);
 
-  return (
+  return loadingPageState !== 1 ? (
+    <div className="min-h-[100vh] min-w-[100vw] p-3 flex flex-col justify-between">
+      <NavigationBar />
+        <Loading />
+        <p></p>
+    </div>
+  ) : (
+    <div className="min-h-[100vh] max-h-[100vh] p-3 flex flex-col justify-between">
+      <NavigationBar />
 
-    loadingPageState !== 1 ? 
-      <div>Loading Data</div>
-    : 
-      <div className="min-h-[100vh] max-h-[100vh] p-3 flex flex-col justify-between">
-        <NavigationBar />
-  
-        {/* main */}
-  
-        <div className="flex flex-col items-center py-2 lg:flex lg:flex-row lg:justify-center lg:items-center lg:my-5 gap-3">
-          <ImgProfile currentBillionaireData={currentBillionaireData}/>
-          <Bio currentBillionaireData={currentBillionaireData} isReadMore={isReadMore} handleReadMoreClick={toggleReadMore}/>
-        </div>
-  
-        <Arrows handleClickNext={onClickNext} handleClickBack={onClickBack}/>
+      {/* main */}
+
+      <div className="flex flex-col items-center py-2 lg:flex lg:flex-row lg:justify-center lg:items-center lg:my-5 gap-3">
+        <ImgProfile currentBillionaireData={currentBillionaireData} />
+        <Bio
+          currentBillionaireData={currentBillionaireData}
+          isReadMore={isReadMore}
+          handleReadMoreClick={toggleReadMore}
+        />
       </div>
-    
 
+      <Arrows handleClickNext={onClickNext} handleClickBack={onClickBack} />
+    </div>
   );
 }
 
---
+++ src/components/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Loading() {
+  return (
+    <div className="text-2xl text-center font-black font-casual">
+      <p>Retrieving Data from API...</p>
+    </div>
+  );
+}
+
+export default Loading;
